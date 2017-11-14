9320ca0ffd21cb8522ef84c6f58881f87538eb52
silencegmt <496555782@qq.com>
2017-11-14 11:31:00 +0800

    web page footer involved

--- js/common.js
+++ js/common.js
@@ -1,9 +1,25 @@
+$(document).ready(function () {
+    $.get('../components/footer.html', function (data) {
+        $('#footer').html(data);
+    });
+    $.get('../components/m-main-footer.html', function (data) {
+        $('#m-main-footer').html(data);
+    });
+});
+
+function changeFooterActive(e, target) {
+    e.preventDefault();
+    $('.footer-nav-ul li').removeClass('custom-nav-active');
+    $(target).closest('li').addClass('custom-nav-active');
+}
+
+
 function backToPrevious() {
     window.history.go(-1);
     return false;
 }
 
 function logInSubmit() {
-    window.location.href("../views/index.html");
+    window.location.href = "../views/index.html";
     return false;
 }
