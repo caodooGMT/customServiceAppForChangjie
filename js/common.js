3446e78d63b09c90802dea65a57ae8115ef11677
silencegmt <496555782@qq.com>
2017-11-24 10:52:04 +0800

    page header

--- js/common.js
+++ js/common.js
@@ -5,6 +5,9 @@ $(document).ready(function () {
     $.get('../components/m-main-footer.html', function (data) {
         $('#m-main-footer').html(data);
     });
+    $.get('../components/header.html', function (data) {
+        $('#header').html(data);
+    });
 });
 
 function backToPrevious() {
@@ -14,5 +17,10 @@ function backToPrevious() {
 
 function logInSubmit() {
     window.location.href = "../views/index.html";
+    return false;
+}
+
+function backToLogIn() {
+    window.location.href = "../views/log-in.html";
     return false;
 }
